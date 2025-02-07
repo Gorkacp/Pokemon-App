ddb0619d1c2d154873930ef2619f6bd6b02cb026
Gorka <gorkacarmonapino@gmail.com>
2025-02-07 20:52:54 +0100

    Actualización 0.6

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Pokemon-App/' // Ajusta el nombre de tu repositorio en GitHub Pages
+  base: './', // Cambia esto para evitar errores con rutas en GitHub Pages
 })
 
